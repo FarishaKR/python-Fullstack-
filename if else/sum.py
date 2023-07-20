b05e2a45f5ba8ce16b49ff65ddc20c810c21340d
Farisha KR <73113669+FarishaKR@users.noreply.github.com>
2023-07-20 20:35:30 +0530

    new
     Write a Python program to calculate the sum of three given numbers, if the values are equal then return three times of their sum.

---
+++ if else/sum.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+num3 = int(input("Enter the number: "))
+
+sum1 = num1 + num2 + num3       
+if num1 == num2 == num3:
+    sum1 = sum1 * 3
+    print(sum1)
+else:
+    print(sum1)
+    
