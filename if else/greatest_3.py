e533c0654e9ef965a818044254f57f774fc5c94a
Farisha KR <73113669+FarishaKR@users.noreply.github.com>
2023-07-20 20:37:22 +0530

    Update greatest_3.py
    Find greatest among three numbers

--- if else/greatest_3.py
+++ if else/greatest_3.py
@@ -1,9 +1,9 @@
-num1 = int(input("Enter the number: "))
+num = int(input("Enter the number: "))
 num2 = int(input("Enter the number: "))
 num3 = int(input("Enter the number: "))
 
-if num1 > num2 and num1 > num3:
-    print(f"{num1} is greatest")
+if num > num2 and num > num3:
+    print(f"{num} is greatest")
 else:
     if num2 > num3:
         print(f"{num2} is greatest")
